fc6c82963331e744ff17ee88c4acf43ae6b153c8
menghuiguli <fujia0@qq.com>
2021-04-12 16:05:20 +0800

    string

--- main.go
+++ main.go
@@ -4,9 +4,12 @@ import (
 	"fmt"
 
 	"github.com/dreamfly2012/go_basic/string"
+	"github.com/google/go-cmp/cmp"
 )
 
 func main() {
 	s := string.ReverseRunes("世界你好!")
 	fmt.Println(s)
+	diff := cmp.Diff("hello world", "hello go")
+	fmt.Println(diff)
 }
