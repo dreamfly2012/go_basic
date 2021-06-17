6c66c9f0629d5ad965cd6190428cdd6727cbd190
menghuiguli <fujia0@qq.com>
2021-06-17 21:50:19 +0800

    add map

--- main.go
+++ main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"fmt"
-	"os"
 
 	"github.com/dreamfly2012/go_basic/string"
+	"github.com/dreamfly2012/go_basic/time"
 	"github.com/google/go-cmp/cmp"
 )
 
@@ -13,29 +13,6 @@ func main() {
 	fmt.Println(s)
 	diff := cmp.Diff("hello world", "hello go")
 	fmt.Println(diff)
-
-	useCAS := false
-	if len(os.Args) > 1 && os.Args[1] == "cas" {
-		useCAS = true
-	}
-	routineCnt := 4
-
-	tryCnt := compareswap.cas._TEST_CNT / routineCnt
-	var obj = &Obj{c: make(chan interface{}, cas._CHAN_SIZE+cas._GUARD_SIZE)}
-
-	for idx := 0; idx < routineCnt; idx++ {
-		go func(nameIdx int) {
-			for tryIdx := 0; tryIdx < tryCnt; tryIdx++ {
-				if useCAS {
-					obj.writeMsgWithCASCheck(nameIdx, nil)
-				} else {
-					obj.writeMsg(nameIdx, nil)
-				}
-			}
-		}(idx)
-	}
-
-	// fmt.Println(casObj.readLoop())
-	fmt.Println(obj.readLoop())
-	fmt.Println("quit.")
+	time := time.FormatTime("2021-10-12 12:12:12")
+	fmt.Println(time)
 }
